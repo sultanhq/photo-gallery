512db6b4d4498e4f87f4a10a6302e1210bff8f54
Rob Adams <radams@findmypast.com>
2017-09-27 14:29:48 +0100

    create imageSource

---
+++ src/assets/ImageSource.js
@@ -0,0 +1,17 @@
+const getImages = () => {
+  let images =  {
+    image1: "http://lorempixel.com/400/200/cats/1/",
+    image2: "http://lorempixel.com/400/200/cats/2/",
+    image3: "http://lorempixel.com/400/200/cats/3/",
+    image4: "http://lorempixel.com/400/200/cats/4/",
+    image5: "http://lorempixel.com/400/200/cats/5/",
+    image6: "http://lorempixel.com/400/200/cats/6/",
+    image7: "http://lorempixel.com/400/200/cats/7/",
+    image8: "http://lorempixel.com/400/200/cats/8/",
+    image9: "http://lorempixel.com/400/200/cats/9/",
+    image10: "http://lorempixel.com/400/200/cats/10/"
+  };
+  return images;
+};
+
+export {getImages};
