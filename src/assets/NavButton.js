6c1b563be700cb6859a697437b996723a3060f5e
Rob Adams <radams@findmypast.com>
2017-09-27 14:30:27 +0100

    NavButton tidied up

--- src/assets/NavButton.js
+++ src/assets/NavButton.js
@@ -1,25 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export class NavButton extends Component {
-
   getButton = () => {
-      const direction = this.props.direction
-      let button = {}
+    const direction = this.props.direction;
+    let button;
     if (direction === "Previous") {
-      button = <button className="Button" >ads{direction}</button>
+      button = <button className="Button">{direction}</button>;
     } else {
-      button = <button className="Button" >{direction}</button>
+      button = <button className="Button">{direction}</button>;
     }
-    return button
-  }
+    return button;
+  };
 
-  render ()
-  {
-
-      return(
-        <div>
-          {this.getButton()}
-        </div>
-      );
+  render() {
+    return <div>{this.getButton()}</div>;
   }
-};
+}
