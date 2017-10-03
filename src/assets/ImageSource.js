d79d85e9a78a36f1db226623ad39a9a4e33fa19c
Rob Adams <radams@findmypast.com>
2017-10-03 11:29:40 +0100

    updated image info

--- src/assets/ImageSource.js
+++ src/assets/ImageSource.js
@@ -1,93 +1,93 @@
 const getData = () => {
   let images = [
     {
-      id: 1,
+      id: 0,
       url: {
         thumb: "http://lorempixel.com/80/60/cats/1/",
         full: "http://lorempixel.com/800/600/cats/1/"
       },
-      title: "CATZ R Gr8",
+      title: "NyanCat",
       caption: "Mewment"
     },
     {
-      id: 2,
+      id: 1,
       url: {
         thumb: "http://lorempixel.com/80/60/cats/2/",
         full: "http://lorempixel.com/800/600/cats/2/"
       },
-      title: "CATZ R Gr8",
+      title: "Pussy Galore",
       caption: "Cat-titude"
     },
     {
-      id: 3,
+      id: 2,
       url: {
         thumb: "http://lorempixel.com/80/60/cats/3/",
         full: "http://lorempixel.com/800/600/cats/3/"
       },
-      title: "CATZ R Gr8",
+      title: "Tiger",
       caption: "Meow-sician"
     },
     {
-      id: 4,
+      id: 3,
       url: {
         thumb: "http://lorempixel.com/80/60/cats/4/",
         full: "http://lorempixel.com/800/600/cats/4/"
       },
-      title: "CATZ R Gr8",
+      title: "Coco",
       caption: "Claw-some"
     },
     {
-      id: 5,
+      id: 4,
       url: {
         thumb: "http://lorempixel.com/80/60/cats/5/",
         full: "http://lorempixel.com/800/600/cats/5/"
       },
-      title: "CATZ R Gr8",
+      title: "Tigger",
       caption: "Paw-lease"
     },
     {
-      id: 6,
+      id: 5,
       url: {
         thumb: "http://lorempixel.com/80/60/cats/6/",
         full: "http://lorempixel.com/800/600/cats/6/"
       },
-      title: "CATZ R Gr8",
+      title: "Felix",
       caption: "Meow-nificent"
     },
     {
-      id: 7,
+      id: 6,
       url: {
         thumb: "http://lorempixel.com/80/60/cats/7/",
         full: "http://lorempixel.com/800/600/cats/7/"
       },
-      title: "CATZ R Gr8",
+      title: "Whiskers",
       caption: "Cathletic"
     },
     {
-      id: 8,
+      id: 7,
       url: {
         thumb: "http://lorempixel.com/80/60/cats/8/",
         full: "http://lorempixel.com/800/600/cats/8/"
       },
-      title: "CATZ R Gr8",
+      title: "Fluffy",
       caption: "Purr-suasion"
     },
     {
-      id: 9,
+      id: 8,
       url: {
         thumb: "http://lorempixel.com/80/60/cats/9/",
         full: "http://lorempixel.com/800/600/cats/9/"
       },
-      title: "CATZ R Gr8",
+      title: "Simba",
       caption: "Paw-sitive"
     },
     {
-      id: 10,
+      id: 9,
       url: {
         thumb: "http://lorempixel.com/80/60/cats/10/",
         full: "http://lorempixel.com/800/600/cats/10/"
       },
-      title: "CATZ R Gr8",
+      title: "Kitty",
       caption: "LOLZ"
     }
   ];
